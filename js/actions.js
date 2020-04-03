4a7deab2610d1dedadbbc43ac332f50b6eb76b0b
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-04-03 21:04:42 +0300

    Header menu on tablet or mobile

--- js/actions.js
+++ js/actions.js
@@ -1,6 +1,25 @@
 "use strict"
 
 // header
+const bars = document.querySelector('#main_header .fa-bars');
+const mainHeader = document.querySelector('#main_header');
+bars.addEventListener('click', function () {
+
+return mainHeader.classList.add("show-menu");
+
+});
+const times = document.querySelector('#main_header .fa-times');
+times.addEventListener('click', function () {
+
+return mainHeader.classList.remove("show-menu");
+
+});
+const overlay = document.querySelector('#main_header .overlay');
+overlay.addEventListener('click', function () {
+
+return mainHeader.classList.remove("show-menu");
+
+});
 
 // hero
 
